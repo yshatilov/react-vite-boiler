6042754b17699afbafb84fbd832244aee7015fb5
yshatilov <49981552+yshatilov@users.noreply.github.com>
2024-12-23 10:55:14 +0000

    Add structure

---
+++ src/mocks/items.ts
@@ -0,0 +1,44 @@
+[
+	{
+		title: "Titile level 1",
+		items: [
+			{
+				title: "Titile level 1.1",
+				items: [
+					{
+						title: "Titile level 1.1.1",
+                        items: [
+                            {
+                                title: "Titile level 1.1.1.1",
+                                items: [
+                                    {
+                                        title: "Titile level 1.1.1.1.1",
+                                    },
+                                    {
+                                        title: "Titile level 1.1.1.1.2",
+                                    }
+                                ]
+                            }
+                        ]
+					},
+					{
+						title: "Titile level 1.1.2",
+					}
+				]
+			}
+		]
+	},
+	{
+		title: "Titile level 2",
+		items: [
+			{
+				title: "Titile level 2.1",
+				items: [
+					{
+					
+					}
+				]
+			}
+		]
+	}
+]
